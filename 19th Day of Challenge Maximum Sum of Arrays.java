9b8f6e085eb867daf07e780d02ba3b0d120a66aa
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2023-11-06 12:16:53 +0530

    Create 19th Day of Challenge Maximum Sum of Arrays.java

---
+++ 19th Day of Challenge Maximum Sum of Arrays.java
@@ -0,0 +1,24 @@
+package com.HundredDays_Challenge.edSlash;
+
+public class DSA_19thDays_Sum_of_Arrays {
+    public static int sumofArrays(int nums[]){
+        int max=nums[0];
+        int currentmax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1]<nums[i]){
+                currentmax=currentmax+nums[i];
+            }
+            else {
+                max = Math.max(max ,currentmax);
+                currentmax=nums[i];
+            }
+        }
+        max=Math.max(max, currentmax);
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,4,6,7,8,3,9,10,2,5};
+        System.out.println(sumofArrays(nums));
+
+    }
+}
