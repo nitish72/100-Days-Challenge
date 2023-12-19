f7f494e62fd5d42d0c99a7b3a4d73d26ab007a08
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2023-12-19 12:39:30 +0530

    Create 41st Day 287. Find the Duplicate Number.java

---
+++ 41st Day 287. Find the Duplicate Number.java
@@ -0,0 +1,23 @@
+//Date : 27th Nove 2023
+//https://leetcode.com/problems/find-the-duplicate-number/
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int ans = 0;
+        for(int i=0; i<nums.length;i++){
+            int ele = nums[i];
+            ele = Math.abs(ele);
+
+            if(nums[ele]>0){
+                nums[ele]=-nums[ele];
+            }else{
+                ans = ele;
+                break;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=Math.abs(nums[i]);
+        }
+        return ans;
+        
+    }
+}
