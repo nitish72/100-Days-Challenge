643a8e24530a0dca53075c9d29e741c1638307ac
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2023-12-22 12:47:52 +0530

    Create 26th Day 35. Search Insert Position.java

---
+++ 26th Day 35. Search Insert Position.java
@@ -0,0 +1,24 @@
+//Date : 13th Nove 2023
+//https://leetcode.com/problems/search-insert-position/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums.length < 0 ){
+            return -1;
+        }
+        int start =0;
+        int end = nums.length - 1;
+        while(start <= end){
+         int mid = start + (end-start)/2;
+         if(nums[mid] < target){
+             start = mid+1;
+             end = end;
+         }else if (nums[mid] > target){
+           end = mid - 1;
+           start = start;
+          }else { //when (mid == target)
+              return mid;  
+               }  
+        }
+        return start;   
+    }
+}
