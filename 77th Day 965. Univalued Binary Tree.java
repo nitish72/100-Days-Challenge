a7aeaf8fa6864d32a447ab26b1833cea7918679d
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2024-01-02 12:35:39 +0530

    Create 77th Day 965. Univalued Binary Tree.java

---
+++ 77th Day 965. Univalued Binary Tree.java
@@ -0,0 +1,33 @@
+// Date : 02 Jan 2024
+//https://leetcode.com/problems/univalued-binary-tree/submissions/1134390764/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean helper(TreeNode root,int val){
+        if(root==null) {
+            return true; 
+    } if(root.val!=val) {
+        return false;
+    }
+    boolean leftans = helper(root.left, val);
+    boolean rightans= helper(root.right,val);
+    return leftans && rightans;
+}
+    public boolean isUnivalTree(TreeNode root) {
+       return helper(root,root.val); 
+    }
+}
