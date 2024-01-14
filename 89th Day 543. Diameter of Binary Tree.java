c850fe8ab623ae2f229f2ca2b4cb0e859f7a9723
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2024-01-14 15:01:53 +0530

    Create 89th Day 543. Diameter of Binary Tree.java

---
+++ 89th Day 543. Diameter of Binary Tree.java
@@ -0,0 +1,41 @@
+// DAte: 14th JAn 2024
+// https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int dia = 0;
+
+    public int height(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+   //CurrNode Diameter = lh + rh;
+        dia = Math.max(lh+rh,dia);
+
+        return Math.max(lh,rh)+1;
+    }  
+    public int diameterOfBinaryTree(TreeNode root)
+{
+   if(root == null){
+       return 0;
+   }
+   int h= height(root);
+   return dia;
+     }        
+}
