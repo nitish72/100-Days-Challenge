6bb9b7252a8a7ead3fd1635d51573067ec2f11a2
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2024-01-18 11:27:49 +0530

    Create 93th Day 701. Insert into a Binary Search Tree.java

---
+++ 93th Day 701. Insert into a Binary Search Tree.java
@@ -0,0 +1,33 @@
+// Date : 18th Jan 2024
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            TreeNode ans = new TreeNode(val);
+            return ans;
+        }
+        if(root.val > val){
+            root.left = insertIntoBST(root.left, val);
+        }else{
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+        
+    }
+}
